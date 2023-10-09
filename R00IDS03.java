86d9734587a47e5d878d3596dff0b4be5e6e492b
Borax-Lemonade <rebeccacan222222@gmail.com>
2023-10-08 22:21:35 -0400

    test for commit

--- R00IDS03.java
+++ R00IDS03.java
@@ -2,11 +2,11 @@
 //   - Do not log unsanitized user input
 
 if (loginSuccessful) {
-  logger.severe("User login succeeded for: " + sanitizeInput(username));
+  logger.severe("User login succeeded for: " + sanitizedInput(username));
 } else {
-  logger.severe("User login failed for: " + sanitizeInput(username));
+  logger.severe("User login failed for: " + sanitizedInput(username));
 }
 
-public String sanitizeInput(String username) {
+public String sanitizedInput(String username) {
     return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
 }
